7ccd3eb4d92e1c9aa9c4f13f1b5e28abef295ef2
Nicolas <ntroccaz@gmail.com>
2022-02-09 18:22:16 +0100

    API Category added 1st draft

---
+++ GestionDrivApi/Controllers/CategoryController.cs
@@ -0,0 +1,138 @@
+﻿using GestionDrivApi.Entities;
+using GestionDrivApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GestionDrivApi.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private CategoryRepository _CategoryRepository;
+
+        public CategoryController(CategoryRepository categoryRepository)
+        {
+            _CategoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> FindAll()
+        {
+            try
+            {
+                List<Category> categories = await _CategoryRepository.FindAll();
+                if (categories == null)
+                    return NotFound();
+                else
+                    return Ok(categories);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> FindById(int id)
+        {
+            try
+            {
+                Category category = await _CategoryRepository.FindById(id);
+                if (category == null)
+                    return NotFound();
+                else
+                    return Ok(category);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{name}")]
+        public async Task<IActionResult> FindByName(string name)
+        {
+            try
+            {
+                Category category = await _CategoryRepository.FindByName(name);
+                if (category == null)
+                    return NotFound();
+                else
+                    return Ok(category);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("rayon/{id}")]
+        public async Task<IActionResult> FindByRayonId(int id)
+        {
+            try
+            {
+                List<Category> categories = await _CategoryRepository.FindByRayonId(id);
+                if (categories == null)
+                    return NotFound();
+                else
+                    return Ok(categories);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpGet("exist/{id:int}")]
+        public async Task<IActionResult> ExistById(int id)
+        {
+            try
+            {
+                bool exist = await _CategoryRepository.ExistById(id);
+                return exist ? Ok(exist) : NotFound(exist);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpGet("exist/{name}")]
+        public async Task<IActionResult> ExistByName(string name)
+        {
+            try
+            {
+                bool exist = await _CategoryRepository.ExistByName(name);
+                return exist ? Ok(exist) : NotFound(exist);
+            }
+            catch (Exception e)
+            {
+                return NotFound(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category newCategory)
+        {
+            try
+            {
+                if (!await _CategoryRepository.ExistByName(newCategory.Nom))
+                {
+
+                    Category category = await _CategoryRepository.Create(newCategory);
+                    return Ok();
+                }
+                else
+                    return BadRequest($"{newCategory.Nom} Category already exists");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
+
---
+++ GestionDrivApi/Repositories/CategoryRepository.cs
@@ -0,0 +1,134 @@
+﻿using GestionDrivApi.Data;
+using GestionDrivApi.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionDrivApi.Repositories
+{
+    public class CategoryRepository
+    {
+        private ApplicationContext _applicationContext;
+
+        public CategoryRepository(ApplicationContext applicationContext)
+        {
+            _applicationContext = applicationContext;
+        }
+
+        public async Task<List<Category>> FindAll()
+        {
+            try
+            {
+                List<Category> categories = await _applicationContext.Categories
+                                            .Include(c => c.Rayon)
+                                            .OrderBy(c => c.Id)
+                                            .ToListAsync();
+                return categories;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public async Task<Category> FindById(int id)
+        {
+            try
+            {
+                Category category = await _applicationContext.Categories
+                                            .Include(c => c.Rayon)
+                                            .Where(c => c.Id == id)
+                                            .SingleAsync();
+                return category;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public async Task<Category> FindByName(string name)
+        {
+            try
+            {
+                Category category = await _applicationContext.Categories
+                                            .Include(c => c.Rayon)
+                                            .Where(c => c.Nom.ToLower() == name.ToLower())
+                                            .SingleAsync();
+                return category;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public async Task<List<Category>> FindByRayonId(int rayonId)
+        {
+            try
+            {
+                List<Category> categories = await _applicationContext.Categories
+                                            .Include(c => c.Rayon)
+                                            .Where(c => c.Rayon.Id == rayonId)
+                                            .OrderBy(c => c.Id)
+                                            .ToListAsync();
+                return categories;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> ExistById(int id)
+        {
+            bool exist = false;
+            try
+            {
+                Category category = await _applicationContext.Categories
+                                            .Include(c => c.Rayon)
+                                            .Where(c => c.Id == id)
+                                            .SingleAsync();
+                return (category != null);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public async Task<bool> ExistByName(string name)
+        {
+            try
+            {
+                Category category = await _applicationContext.Categories
+                                            .Include(c => c.Rayon)
+                                            .Where(c => c.Nom.ToLower() == name.ToLower())
+                                            .SingleAsync();
+                return (category != null);
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+
+        public async Task<Category> Create(Category newCategory)
+        {
+            try
+            {
+                newCategory.Rayon = new Rayon();
+                await _applicationContext.Categories.AddAsync(newCategory);
+                await _applicationContext.SaveChangesAsync();
+                return newCategory;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
+
