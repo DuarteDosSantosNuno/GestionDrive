92fa36a245703258e4c55916a82cb155d9b1e5d4
Cedric <cedric_elong@yahoo.it>
2022-02-15 09:15:08 +0100

    New BackOffice - React

--- backOffice/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace backOffice.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
