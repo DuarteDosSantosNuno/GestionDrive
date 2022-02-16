e66f664727bb11702531df1942021bd557cb0284
rimond.thanawan <rimond.thanawan@hotmail.com>
2022-02-16 15:53:40 +0100

    productCard update (front office)

--- front-office/src/components/ProductCard.jsx
+++ front-office/src/components/ProductCard.jsx
@@ -12,12 +12,19 @@ import { BASE_IMAGE_URL } from "./../APIConfig";
 
 export default function ProductCard(props) {
   const { product, onAdd } = props;
+  let pathImage;
+
+  if(product.productImages.length === 0)
+    pathImage = "./tmp/img/noimage.png";
+  else
+    pathImage = BASE_IMAGE_URL + product.productImages[0].src;
   
   return (
     <MDBCard className="h-100 shadow-custom" style={{ maxWidth: "16rem" }}>
       <div className="d-flex align-items-center justify-content-center">
-        <MDBCardImage
-          src={`${BASE_IMAGE_URL}${product.productImages[0].src}`}
+        <MDBCardImage 
+          //src={a ? `${BASE_IMAGE_URL}${product.productImages[0].src}` : `${img}`}
+          src={pathImage}
           alt="..."
           position="top"
           style={{ maxWidth: "7rem" }}
@@ -27,7 +34,7 @@ export default function ProductCard(props) {
       </div>
       <MDBCardBody>
         <MDBCardTitle>{product.nom}</MDBCardTitle>
-        <p className="text-muted">{product.units[0].prix} €/piece</p>
+        <p className="text-muted">{product.units[0].prix} €/{product.units[0].unite}</p>
         <div className="d-flex align-items-center justify-content-center">
           <MDBBtn onClick={onAdd}>{props.btn}</MDBBtn>
         </div>
